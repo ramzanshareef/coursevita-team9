cd675f3db1662c4a325579eac59be771d093747d
Banda Sujith Kumar <sujith.suave@gmail.com>
2024-09-15 15:01:43 +0530

    CHORE: Added middleware.

---
+++ middleware.ts
@@ -0,0 +1,20 @@
+import { authMiddleware } from "@clerk/nextjs";
+import { pathToRegexp } from "path-to-regexp";
+
+export default authMiddleware({
+    publicRoutes: [
+        "/",
+        "/api/webhook/clerk",
+        "/api/uploadthing",
+        pathToRegexp("/post/:id")
+    ],
+    ignoredRoutes: [
+        "/api/webhook/clerk",
+        "/api/uploadthing",
+        "/api/test",
+    ],
+});
+
+export const config = {
+    matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
+}
