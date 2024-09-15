b068519ad366346f14a228084575c943b02b044d
Banda Sujith Kumar <sujith.suave@gmail.com>
2024-09-15 14:50:29 +0530

    FEAT: User can now create communities and other user can join communities.

---
+++ app/community/[id]/layout.tsx
@@ -0,0 +1,23 @@
+import { auth } from "@clerk/nextjs";
+import Sidebar from "../../(root)/Sidebar";
+import { getCommunitiesJoinedByUser, getUser } from "@/lib/actions/user.actions";
+
+export default async function RootLayout({
+    children,
+}: Readonly<{
+    children: React.ReactNode;
+}>) {
+    const {userId} = auth();
+    const currentUser = await getUser(userId);
+    const { data, status } = await getCommunitiesJoinedByUser(currentUser);
+    return (
+        <>
+            <main className="pt-16 p-4 bg-gray-100 h-screen overflow-y-scroll scrollbar-hidden">
+                <div className="flex flex-row gap-4 mt-2 *:min-h-full">
+                    <Sidebar communitites={status === 200 ? data : []} />
+                    <div className="w-full">{children}</div>
+                </div>
+            </main>
+        </>
+    );
+}
