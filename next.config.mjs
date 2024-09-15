ee53b1042d219453c08e7fbba038eafb9f8dfd79
Banda Sujith Kumar <sujith.suave@gmail.com>
2024-09-15 14:34:11 +0530

    CHORE: Changes in nextjs configuration.

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+/** @type {import("next").NextConfig} */
+const nextConfig = {
+    images: {
+        remotePatterns: [
+            {
+                protocol: "https",
+                hostname: "**",
+            },
+        ]
+    }
+};
 
 export default nextConfig;
